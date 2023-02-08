f52465ecee34f232495b51073bf8ba1aeeb4f94c
Lucas dos Santos Alves <30706052+lucasdsalves@users.noreply.github.com>
2023-02-08 17:48:44 -0300

    Add domain event

---
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -0,0 +1,33 @@
+import EventDispatcherInterface from "./event-dispatcher.interface";
+import EventHandlerInterface from "./event-handler.interface";
+import EventInterface from "./event.interface";
+
+export default class EventDispatcher implements EventDispatcherInterface {
+  private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+  get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+    return this.eventHandlers;
+  }
+
+  notify(event: EventInterface): void {}
+
+  register(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = [];
+    }
+    this.eventHandlers[eventName].push(eventHandler);
+  }
+
+  unregister(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (this.eventHandlers[eventName]) {
+      const index = this.eventHandlers[eventName].indexOf(eventHandler);
+      if (index !== -1) {
+        this.eventHandlers[eventName].splice(index, 1);
+      }
+    }
+  }
+
+  unregisterAll(): void {
+    this.eventHandlers = {};
+  }
+}
