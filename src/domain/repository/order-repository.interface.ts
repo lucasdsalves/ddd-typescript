acefd224a3b1ad0e3f99550c9ade214a028b1107
Lucas dos Santos Alves <30706052+lucasdsalves@users.noreply.github.com>
2023-02-09 17:35:17 -0300

    Add modules

--- src/domain/repository/order-repository.interface.ts
+++
@@ -1,5 +0,0 @@
-import { Order } from "sequelize";
-import RepositoryInterface from "./repository.interface";
-
-export default interface OrderRepositoryInterface extends
-RepositoryInterface<Order>{}
